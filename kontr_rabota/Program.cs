d4013e1e0fa9322923ef26b2636c0d9e56e0e972
Artem <tomilow.art@yandex.ru>
2022-11-14 19:41:24 +1200

    Program.cs

--- kontr_rabota/Program.cs
+++ kontr_rabota/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string [] stringArray = new string [n];
+void FillArray(string [] stringArray)
+{
+ for (int i = 0; i < stringArray.Length; i++)
+ {
+ stringArray[i] = Console.ReadLine();
+ }
+}
+void PrintArray(string [] stringArray)
+{
+ for (int i = 0; i < stringArray.Length; i++)
+ {
+ Console.Write($"{stringArray [i]} ");
+ }
+}
+FillArray(stringArray);
+Console.WriteLine("Исходный массив:");
+PrintArray(stringArray);
+Console.WriteLine();
+void StrArray(string [] stringArray)
+{
+ for (int i = 0; i < stringArray.Length; i++)
+ {
+    int count = stringArray[i].Count();
+    if (count < 3)
+    {
+        Console.Write($"{stringArray [i]} ");
+        count = 0;
+    }
+ }
+}
+Console.WriteLine("Новый массив:");
+StrArray(stringArray);
